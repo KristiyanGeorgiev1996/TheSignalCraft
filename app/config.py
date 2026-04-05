3dc586bbf1afbb4c4b7cf31ed6e7f844f019ff6e
Kristiyan Georgiev <kristiqn35@yahoo.com>
2026-04-05 14:07:42 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,1 +1,7 @@
+import os
 
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+if not TELEGRAM_TOKEN:
+    raise ValueError("TELEGRAM_TOKEN is not set. Please add it to your environment variables.")
